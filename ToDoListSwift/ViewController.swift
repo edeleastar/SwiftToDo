56f40072cc4d3fb990085338ee4691965ed11dc6
Eamonn de Leastar <edeleastar@gmail.com>
2014-06-21 12:07:14 +0100

    Text Field in View + delegate implementation in controller

--- ToDoListSwift/ViewController.swift
+++ ToDoListSwift/ViewController.swift
@@ -1,12 +1,21 @@
 import UIKit
 
-class ViewController: UIViewController
+class ViewController: UIViewController, UITextFieldDelegate
 {
+  @IBOutlet var toDoItem: UITextField
+  
   override func viewDidLoad()
   {
     super.viewDidLoad()
+    toDoItem.delegate = self
   }
-
+  
+  func textFieldShouldReturn(textField: UITextField!) -> Bool
+  {
+    println(toDoItem.text)
+    return true
+  }
+  
   override func didReceiveMemoryWarning()
   {
     super.didReceiveMemoryWarning()
