8aae322fa8d3e981038f5838ab290d0738f119bb
Eamonn de Leastar <edeleastar@gmail.com>
2014-06-22 07:41:59 +0100

    The bridging file itself

---
+++ ToDoListSwift/ToDoListController.h
@@ -0,0 +1,10 @@
+#import <Foundation/Foundation.h>
+
+#import <UIKit/UIKit.h>
+
+class ToDoListController
+@interface ToDoListController
+
+- (IBAction)unwindToList: (UIStoryboardSegue *)segue;
+
+@end
